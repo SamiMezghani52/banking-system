1de16f61ae918486c0f1f9cd91c0249727b9d54d
Sami Mezghani <s.mezghani@voguelconsulting.com>
2024-05-22 11:55:49 +0100

    another test

--- accounts/tests.py
+++ accounts/tests.py
@@ -2,4 +2,4 @@ from django.test import TestCase
 
 # Create your tests here.
 
-# Theres plenty of tests scenarios that can be tested in Here ("This comment is for sonarQube analysis purpose")
+# ("This comment is for sonarQube analysis purpose")
